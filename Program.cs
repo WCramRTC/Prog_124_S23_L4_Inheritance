76bb562035c381db238948ded2e78e32a79b5f71
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-04-16 12:57:25 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_124_S23_L4_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
